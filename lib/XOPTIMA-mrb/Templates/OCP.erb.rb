c5481d9fc413a84bf2148fd6b41e18a0e4a0c47c
max-codeware <max.codeware@gmail.com>
2020-05-18 21:48:34 +0200

    Enclosed template in a string

--- lib/XOPTIMA-mrb/Templates/OCP.erb.rb
+++ lib/XOPTIMA-mrb/Templates/OCP.erb.rb
@@ -1,3 +1,39 @@
+# Copyright (c) 2020, Massimiliano Dal Mas
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+# 
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the distribution
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+##
+# This is the ERB template for generating the file
+# `OCP.rb`. All the template names correspond to
+# a method in the class `Poblem`. This consistency must be
+# strictrly followed. This template is rendered in
+# `FileGenerator#generate_ocp_rb`. All the parameters
+# passed to `ocp_render` appear in the corresponding order
+# they appear here. 
+# For any modification, update `ocp_render.render` arguments
+# and the list of names in `GeneratorHelper::OCPNameSet`.
+XOPTIMA::OCPrb = <<T
 class COMMON_Build
   DATA = {}
   DATA[:numThread]            = <%=num_thread %>
@@ -152,3 +188,4 @@ class COMMON_Build
   ]
 
 end
+T
