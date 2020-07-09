0f4d117f6d4f3924d2a4b2dc52cad5f5560327a9
max-codeware <max.codeware@gmail.com>
2020-07-09 21:45:43 +0200

    Improved and added more routines for file generation

--- lib/XOPTIMA-mrb/Generator/FileGenerator.rb
+++ lib/XOPTIMA-mrb/Generator/FileGenerator.rb
@@ -43,25 +43,123 @@ module XOPTIMA
   	end
 
     def render_files
+      @dict = @ocproblem.substitution_dict
       generate_ocp_rb
+      generate_sparse_mx_files
+      generate_precalculated
+      generate_rhs_ode
+      generate_checks
     end
     
     ##
     # Renders and generates the file `OCP.rb`
     def generate_ocp_rb
-      print "Rendering `OCP.rb'..." if @ocproblem.verbose
+      log_report("OCP.rb") do
       
-      # Loading templates and creating renders
-      ocp_rb_render         = make_template_render(OCPrb, OCPNameSet)
-      user_f_class_i_render = make_template_render(UserFunctionsCI, UserFCINameSet)
+        # Loading templates and creating renders
+        ocp_rb_render         = make_template_render(OCPrb, OCPNameSet)
+        user_f_class_i_render = make_template_render(UserFunctionsCI, UserFCINameSet)
 
-      # Rendering content of `DATA[:UserFunctionsClassInstances]`
-      user_f_class_i = render_user_functions_ci(user_f_class_i_render)
+        # Rendering content of `DATA[:UserFunctionsClassInstances]`
+        user_f_class_i = render_user_functions_ci(user_f_class_i_render)
       
-      # Rendering `OCP.rb'
-      ocp_rb = ocp_rb_render.render(@ocproblem, user_f_class_i)
+        # Rendering `OCP.rb'
+        ocp_rb = ocp_rb_render.render(@ocproblem, user_f_class_i)
 
-      open("OCP_tmp/OCP.rb", "w") { |io| io.puts ocp_rb }
+        open("OCP_tmp/OCP.rb", "w") { |io| io.puts ocp_rb }
+      end
+    end
+
+    ##
+    # Generates all the files of the sparse matrices
+    def generate_sparse_mx_files
+      @ocproblem.sparse_mxs.each do |mx|
+        write_matrix("#{mx.label}.c_code", mx, @dict)
+      end
+    end
+    
+    def generate_precalculated
+      write_with_log("eta.c_code") do |io| 
+        write_array(@ocproblem.eta, io)
+      end
+
+      write_with_log("g.c_code") do |io| 
+        write_array(@ocproblem.g, io)
+      end
+
+      write_with_log("jump.c_code") do |io|
+        write_array(@ocproblem.jump, io)
+      end 
+
+      write_with_log("nu.c_code") do |io|
+        write_array(@ocproblem.nu, io)
+      end
+
+      write_with_log("H_fun.c_code") do |io|
+        io << "result__ = #{@ocproblem.H.subs(@dict)}"
+      end
+    end
+
+    def generate_rhs_ode
+      write_with_log("rhs_ode.c_code") do |io|
+        write_array(@ocproblem.rhs, io)
+      end
+    end
+
+    def generate_checks
+      write_with_log("cell_check.c_code") do |io|
+      end
+
+      write_with_log("node_check.c_code") do |io|
+      end
+
+      write_with_log("pars_check.c_code") do |io|
+      end
+
+      write_with_log("u_check.c_code") do |io|
+      end
+    end
+
+    def generate_guess
+      write_with_log("l_guess.c_code") do |io|
+      end
+
+      write_with_log("u_guess.c_code") do |io|
+      end
+
+      write_with_log("x_guess.c_code") do |io|
+      end
+
+      write_with_log("p_guess.c_code") do |io|
+      end
+    end
+
+  private 
+
+    def write_array(ary, io)
+      ary.each_with_index do |el, i|
+        io.puts "result__[#{i}] = #{el.subs(@dict)}"
+      end 
+    end
+
+    def write_matrix(name, mx, dict)
+      write_with_log(name) do |io| 
+        i = -1
+        mx.each_value do |v|
+          io.puts "result__[#{i += 1}] = #{v.subs(dict)}"
+        end
+      end
+    end
+
+    def write_with_log(name)
+      log_report(name) do
+        open("OCP_tmp/#{name}", "w") { |io| yield io }
+      end
+    end
+
+    def log_report(file)
+      print "Rendering `#{file}'..." if @ocproblem.verbose
+      yield
       puts "done!" if @ocproblem.verbose
     end
   end
