b8c00d67b8bd0756b1b310355f277cb05e23924b
Massimiliano Dal Mas <max.codeware@gmail.com>
2020-07-23 19:14:23 +0200

    Created module for command line execution

---
+++ lib/XOPTIMA-mrb/Command/Command.rb
@@ -0,0 +1,38 @@
+# Copyright (c) 2020, Massimiliano Dal Mas
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+# 
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in
+#       the documentation and/or other materials provided with the distribution
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+module XOPTIMA
+	module Command
+		class CommandError < RuntimeError; end
+
+    def self.generate_problem
+      #Dir.chdir("./OCP_tmp")
+      cmd = "pins /Library/Frameworks/Maple.framework/Versions/Current/toolbox/XOptima/XOptima_GenerateCppCode_OCP.rb BangBang --output_directory=./"
+      if !system(cmd)
+      	raise CommandError, $?
+      end
+    end
+	end
+end
