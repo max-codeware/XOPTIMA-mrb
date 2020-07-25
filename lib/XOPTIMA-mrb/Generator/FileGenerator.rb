7d92550b5d66df00a369e23ed14680f35e5fe5aa
Massimiliano Dal Mas <max.codeware@gmail.com>
2020-07-25 17:14:00 +0200

    Fixed code style consistency

--- lib/XOPTIMA-mrb/Generator/FileGenerator.rb
+++ lib/XOPTIMA-mrb/Generator/FileGenerator.rb
@@ -69,7 +69,7 @@ module XOPTIMA
     ##
     # It renders and generates the file `OCP.rb`
     def generate_ocp_rb
-      log_report("OCP.rb") do
+      __log_report("OCP.rb") do
       
         # Loading templates and creating renders
         ocp_rb_render         = make_template_render(OCPrb, OCPNameSet)
@@ -89,7 +89,7 @@ module XOPTIMA
     # It generates all the files of the sparse matrices
     def generate_sparse_mx_files
       @ocproblem.sparse_mxs.each do |mx|
-        write_matrix("#{mx.label}.c_code", mx, @dict)
+        __write_matrix("#{mx.label}.c_code", mx, @dict)
       end
     end
     
@@ -101,23 +101,23 @@ module XOPTIMA
     #   * nu
     #   * H
     def generate_precalculated
-      write_with_log("eta.c_code") do |io| 
-        write_array(@ocproblem.eta, io)
+      __write_with_log("eta.c_code") do |io| 
+        __write_array(@ocproblem.eta, io)
       end
 
-      write_with_log("g.c_code") do |io| 
-        write_array(@ocproblem.g, io)
+      __write_with_log("g.c_code") do |io| 
+        __write_array(@ocproblem.g, io)
       end
 
-      write_with_log("jump.c_code") do |io|
-        write_array(@ocproblem.jump, io)
+      __write_with_log("jump.c_code") do |io|
+        __write_array(@ocproblem.jump, io)
       end 
 
-      write_with_log("nu.c_code") do |io|
-        write_array(@ocproblem.nu, io)
+      __write_with_log("nu.c_code") do |io|
+        __write_array(@ocproblem.nu, io)
       end
 
-      write_with_log("H_fun.c_code") do |io|
+      __write_with_log("H_fun.c_code") do |io|
         io.puts "result__ = #{@ocproblem.H.subs(@dict)};"
       end
     end
@@ -125,24 +125,24 @@ module XOPTIMA
     ##
     # It generates the file for the right-hand side of the ODE
     def generate_rhs_ode
-      write_with_log("rhs_ode.c_code") do |io|
-        write_array(@ocproblem.rhs, io)
+      __write_with_log("rhs_ode.c_code") do |io|
+        __write_array(@ocproblem.rhs, io)
       end
     end
 
     ##
     # It generates the files for checks
     def generate_checks
-      write_with_log("cell_check.c_code") do |io|
+      __write_with_log("cell_check.c_code") do |io|
       end
 
-      write_with_log("node_check.c_code") do |io|
+      __write_with_log("node_check.c_code") do |io|
       end
 
-      write_with_log("pars_check.c_code") do |io|
+      __write_with_log("pars_check.c_code") do |io|
       end
 
-      write_with_log("u_check.c_code") do |io|
+      __write_with_log("u_check.c_code") do |io|
         @ocproblem.control_bounds.each_with_index do |cb,i|
           cb_x = @dict[cb.control[@ocproblem.independent]]
           Check.not_nil(cb_x)
@@ -154,10 +154,10 @@ module XOPTIMA
     ##
     # It generates the files for guesses
     def generate_guess
-      write_with_log("l_guess.c_code") do |io|
+      __write_with_log("l_guess.c_code") do |io|
       end
 
-      write_with_log("u_guess.c_code") do |io|
+      __write_with_log("u_guess.c_code") do |io|
         # TODO: temporary workaround. Needs to be implemented
         io.puts "result[0] = 0;"
       end
@@ -167,7 +167,7 @@ module XOPTIMA
       # Then, it is substituted into the expression after the
       # dictionary for code generation (we want to subs only the remaining
       # independent variable stated as free one)
-      write_with_log("x_guess.c_code") do |io|
+      __write_with_log("x_guess.c_code") do |io|
         io.puts "t1 = Q__[0];"
         states = @ocproblem.states
         dict   = {@ocproblem.independent => var(:t1)}
@@ -177,7 +177,7 @@ module XOPTIMA
         end
       end
 
-      write_with_log("p_guess.c_code") do |io|
+      __write_with_log("p_guess.c_code") do |io|
       end
     end
 
@@ -187,16 +187,16 @@ module XOPTIMA
     #   * dh/du
     #   * dH/dx
     def generate_H_files
-      write_with_log("Hp.c_code") do |io|
-        write_array(@ocproblem.dH_dp, io)
+      __write_with_log("Hp.c_code") do |io|
+        __write_array(@ocproblem.dH_dp, io)
       end
 
-      write_with_log("Hu.c_code") do |io|
-        write_array(@ocproblem.dH_du, io)
+      __write_with_log("Hu.c_code") do |io|
+        __write_array(@ocproblem.dH_du, io)
       end
 
-      write_with_log("Hx.c_code") do |io|
-        write_array(@ocproblem.dH_dx, io)
+      __write_with_log("Hx.c_code") do |io|
+        __write_array(@ocproblem.dH_dx, io)
       end
     end
 
@@ -204,11 +204,11 @@ module XOPTIMA
     # It generates the files for the lagrange and
     # mayer targets
     def generate_targets
-      write_with_log("mayer_target.c_code") do |io|
+      __write_with_log("mayer_target.c_code") do |io|
         io.puts "result__ = #{@ocproblem.mayer.subs(@dict)};"
       end
 
-      write_with_log("lagrange_target.c_code") do |io|
+      __write_with_log("lagrange_target.c_code") do |io|
         io.puts "result__ = #{@ocproblem.lagrange.subs(@dict)};"
       end
     end
@@ -217,7 +217,7 @@ module XOPTIMA
     # It generates the `bc` file where the
     # boundary conditions are coded
     def generate_bc
-      write_with_log("bc.c_code") do |io|
+      __write_with_log("bc.c_code") do |io|
         i = -1
         @ocproblem.final.each do |k, v|
           exp = k[@ocproblem.right] - v 
@@ -236,8 +236,8 @@ module XOPTIMA
     ##
     # It generates the `adjointBC` file
     def generate_adjointBC
-      write_with_log("adjointBC.c_code") do |io|
-        write_array(@ocproblem.adjointBC, io)
+      __write_with_log("adjointBC.c_code") do |io|
+        __write_array(@ocproblem.adjointBC, io)
       end
     end
 
@@ -245,10 +245,10 @@ module XOPTIMA
     # It generates the `post` and integrated_post`
     # files
     def generate_post
-      write_with_log("post.c_code") do |io|
+      __write_with_log("post.c_code") do |io|
       end
 
-      write_with_log("integrated_post.c_code") do |io|
+      __write_with_log("integrated_post.c_code") do |io|
       end
     end
 
@@ -256,11 +256,11 @@ module XOPTIMA
     # It generates the `Jp_controls` and `Jp_fun`
     # files
     def generate_jp
-      write_with_log("Jp_controls.c_code") do |io|
+      __write_with_log("Jp_controls.c_code") do |io|
         io.puts "result__ = #{@ocproblem.P.subs(@dict)};"
       end
 
-      write_with_log("Jp_fun.c_code") do |io|
+      __write_with_log("Jp_fun.c_code") do |io|
         # TODO: this part must be implemented
         io.puts "result__ = 0;"
       end
@@ -269,11 +269,11 @@ module XOPTIMA
     ##
     # It generates the `q` and `u` file
     def generate_q_u
-      write_with_log("q.c_code") do |io|
+      __write_with_log("q.c_code") do |io|
         io.puts "result__[0] = s;"
       end
 
-      write_with_log("u.c_code") do |io|
+      __write_with_log("u.c_code") do |io|
         io.puts "// Solver not implemented in Ruby"
         @ocproblem.controls.each_with_index do |c, i|
           io.puts "result__[#{i}] = 0;"
@@ -284,7 +284,7 @@ module XOPTIMA
     ##
     # It generates the `DuDxlp` file
     def generate_DuDxlp
-      write_with_log("DuDxlp.c_code") do |io|
+      __write_with_log("DuDxlp.c_code") do |io|
         io.puts "LW_ERROR0(\"DuDxlp not defined\");"
       end
     end
@@ -294,7 +294,7 @@ module XOPTIMA
     ##
     # It writes a vector saved as an array as 
     # `result__[j] = compj` into the given `io`
-    def write_array(ary, io)
+    def __write_array(ary, io)
       ary.each_with_index do |el, i|
         io.puts "result__[#{i}] = #{el.subs(@dict)};"
       end 
@@ -303,8 +303,8 @@ module XOPTIMA
     ##
     # It writes the non-zero components of a sparse matrix as 
     # `result__[j] = compj` into the given `io`
-    def write_matrix(name, mx, dict)
-      write_with_log(name) do |io| 
+    def __write_matrix(name, mx, dict)
+      __write_with_log(name) do |io| 
         i = -1
         mx.each_value do |v|
           io.puts "result__[#{i += 1}] = #{v.subs(dict)};"
@@ -316,8 +316,8 @@ module XOPTIMA
     # It creates a file with the given name passing the
     # `io` buffer to the given block and printing a log message
     # to the console
-    def write_with_log(name)
-      log_report(name) do
+    def __write_with_log(name)
+      __log_report(name) do
         open("OCP_tmp/#{name}", "w") { |io| yield io }
       end
     end
@@ -325,7 +325,7 @@ module XOPTIMA
     ##
     # It prints a log message to the console before executing the
     # given block and after. This is used for debug purposes
-    def log_report(file)
+    def __log_report(file)
       print "Rendering `#{file}'..." if @ocproblem.verbose
       yield
       puts "done!" if @ocproblem.verbose
