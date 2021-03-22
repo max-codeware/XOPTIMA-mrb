6c92f1ac12b8e9b477cd12d342e8fb7d10dcd3a7
Massimiliano Dal Mas <max.codeware@gmail.com>
2021-03-22 17:33:51 +0100

    Fixed copyright date

--- lib/XOPTIMA-mrb/Generator/FileGenerator.rb
+++ lib/XOPTIMA-mrb/Generator/FileGenerator.rb
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, Massimiliano Dal Mas
+# Copyright (c) 2020-2021, Massimiliano Dal Mas
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
